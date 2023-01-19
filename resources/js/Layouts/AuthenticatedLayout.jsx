6def11d82ed5d0dd333e684c3fdb0fda9260004a
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-19 10:45:19 +0200

    Added "Find Work" NavLink on the AuthenticatedLayout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ auth, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('jobs.index')} active={route().current('jobs.index')}>
+                                    Find Jobs
+                                </NavLink>
                             </div>
                         </div>
 
@@ -94,6 +97,9 @@ export default function Authenticated({ auth, header, children }) {
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink href={route('jobs.index')} active={route().current('jobs.index')}>
+                            Find Jobs
                         </ResponsiveNavLink>
                     </div>
 
