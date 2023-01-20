7c1653eb30b8a1721b8203348efefc5b7453613a
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-20 16:16:22 +0200

    Auth middleware exception for index/show routes in JobController

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -9,6 +9,11 @@ use Inertia\Inertia;
 
 class JobController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
