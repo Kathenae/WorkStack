80c5b20fd639d292c2744507dbeab9a03bc84898
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-21 17:21:35 +0200

    Use existing Job component on Job Details Page

--- resources/js/Pages/Jobs/Show.jsx
+++ resources/js/Pages/Jobs/Show.jsx
@@ -1,42 +1,11 @@
 import React from 'react';
-import SkillBadge from '@/Components/SkillBadge';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import dayjs from 'dayjs';
-import { useState } from 'react';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import Job from '@/Components/Job';
 
-dayjs.extend(relativeTime)
 
 export default function Show({ job, auth, errors: auth_errors }) {
 
-    const [isShowingMore, setIsShowingMore] = useState(false);
-
-    const truncate = (text, length = 450) => {
-        if (text.length <= length) {
-            return text;
-        }
-
-        const new_text = text.slice(0, length);
-        const lastSpaceIndex = new_text.lastIndexOf(" ");
-        return new_text.slice(0, lastSpaceIndex) + "...";
-    };
-
-    const status = (status) => {
-        let extraClasses = {
-            'open': 'text-indigo-500',
-            'closed': 'text-gray-300',
-            'canceled': 'text-red-500',
-            'completed': 'text-green-400',
-        }
-
-        return (
-            <a
-                href='/skills/frontend'
-                className={'bg-transparent font-extrabold cursor-pointer rounded-xl ' + extraClasses[status]}>
-                {status.charAt(0).toUpperCase() + status.slice(1)}
-            </a>)
-    }
     return (
         <AuthenticatedLayout
             auth={auth}
@@ -46,40 +15,7 @@ export default function Show({ job, auth, errors: auth_errors }) {
             <Head title="Show Job" />
 
             <div className="pt-12 pb-6">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            <div className="p-6 text-gray-900">
-                                <h2 className='font-semibold text-xl leading-tight mt-6'>
-                                    {job.title}
-                                </h2>
-                                <small className='text-gray-400 text-sm mt-3'>
-                                    {job.type == 'fixed_price' ? "Fixed Price" : "Hourly"}
-                                    : ${job.min_price}-${job.max_price}
-                                    - Intermediate Level
-                                    <br />
-                                    Posted {dayjs(job.created_at).fromNow()}
-                                    - {status(job.status)}
-                                </small>
-                                <p className='mt-6 whitespace-pre-line'>{isShowingMore ? job.description : truncate(job.description)}</p>
-                                {job.description.length > 450 &&
-                                    <span
-                                        onClick={e => setIsShowingMore(!isShowingMore)}
-                                        className='font-bold text-indigo-700 hover:underline hover:cursor-pointer'
-                                    >
-                                        {isShowingMore ? "Less" : "More"}
-                                    </span>
-                                }
-
-                                <div className='mt-4'>
-                                    {job.skills.map((skill) => {
-                                        return <SkillBadge key={skill.id} skill={skill} />
-                                    })}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Job job={job} />
             </div>
         </AuthenticatedLayout>
     );
