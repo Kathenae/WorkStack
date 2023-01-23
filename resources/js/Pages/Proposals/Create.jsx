65bbc711a700675ea53c1e8c7da0d6d76ff215f6
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-23 22:21:29 +0200

    Added Create and Show front-end pages

---
+++ resources/js/Pages/Proposals/Create.jsx
@@ -0,0 +1,35 @@
+import ProposalForm from '@/Components/ProposalForm';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head, useForm } from "@inertiajs/react";
+import React from "react";
+
+export default function Create({ job, auth, errors: auth_errors }) {
+    const form = useForm({
+        coverLetter: '',
+        price: ''
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        form.post(route('jobs.proposals.store', job.id))
+    }
+
+    return (
+        <AuthenticatedLayout
+            auth={auth}
+            errors={auth_errors}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Create Job</h2>}
+        >
+            <Head title="Create Job" />
+            <div className="pt-12 pb-6">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <ProposalForm form={form} onSubmit={handleSubmit} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </ AuthenticatedLayout >
+    )
+}
