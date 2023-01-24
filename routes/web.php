a9489ff25c60955d3b6f1eb108d0635a0ca24a98
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-24 13:11:48 +0200

    Fix Proposal routes to require authentication

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('jobs', JobController::class);
-Route::resource('jobs.proposals', ProposalController::class)->middleware('auth');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('jobs.proposals', ProposalController::class)->only(['index', 'create', 'store']);
+    Route::resource('proposals', ProposalController::class)->except(['index', 'create', 'store']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
