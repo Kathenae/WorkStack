e32b4c177bd76fa940f5dc77647dbaa693436f59
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-24 13:09:58 +0200

    Minor Refactor on /Proposals/Create.jsx

--- resources/js/Pages/Proposals/Create.jsx
+++ resources/js/Pages/Proposals/Create.jsx
@@ -18,9 +18,9 @@ export default function Create({ job, auth, errors: auth_errors }) {
         <AuthenticatedLayout
             auth={auth}
             errors={auth_errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Create Job</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Create Proposal</h2>}
         >
-            <Head title="Create Job" />
+            <Head title="Create Proposal" />
             <div className="pt-12 pb-6">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
