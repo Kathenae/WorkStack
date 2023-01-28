f913b7b69b2e031c46b41980c8e364c2033ec821
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-28 13:33:58 +0200

    Update Dashboard.jsx to show a list of the users jobs and proposals

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,22 +1,51 @@
+import Card from '@/Components/Card';
+import Conditional from '@/Components/Conditional';
+import Container from '@/Components/Container';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
+import { useState } from 'react';
 
-export default function Dashboard(props) {
+export default function Dashboard({ auth, errors, jobs, proposals }) {
+
+    const [isJobListOpen, setIsJobListOpen] = useState(false)
+    const [isProposalListOpen, setisProposalListOpen] = useState(false)
+
     return (
         <AuthenticatedLayout
-            auth={props.auth}
-            errors={props.errors}
+            auth={auth}
+            errors={errors}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Card className='mb-4 py-0'>
+                    <Card.Content>
+                        <div className="flex justify-between">
+                            <h2 className="font-semibold text-xl text-gray-800 leading-tight">Jobs ({jobs.length})</h2>
+                            <button onClick={e => setIsJobListOpen(!isJobListOpen)}><i className={"fi fi-rs-angle-" + (isJobListOpen ? "down" : "right")}></i></button>
+                        </div>
+                        <Conditional showIf={isJobListOpen}>
+                            <div className="mt-8">
+                                {jobs.map(job => <h4 key={job.id} className='font-semibold text-indigo-500 py-4 border-t'><Link className='hover:underline' href={route('jobs.show', job.id)} >{job.title}</Link></h4>)}
+                            </div>
+                        </Conditional>
+                    </Card.Content>
+                </Card>
+                <Card className='mb-4 mt-0 py-0'>
+                    <Card.Content>
+                        <div className="flex justify-between">
+                            <h2 className="font-semibold text-xl text-gray-800 leading-tight">Proposals ({proposals.length})</h2>
+                            <button onClick={e => setisProposalListOpen(!isProposalListOpen)}><i className={"fi fi-rs-angle-" + (isProposalListOpen ? "down" : "right")}></i></button>
+                        </div>
+                        <Conditional showIf={isProposalListOpen}>
+                            <div className="mt-8">
+                                {proposals.map(proposal => <h4 key={proposal.id} className='font-semibold text-indigo-500 py-4 border-t'><Link className='hover:underline' href={route('proposals.show', proposal.id)}>{proposal.job.title}</Link></h4>)}
+                            </div>
+                        </Conditional>
+                    </Card.Content>
+                </Card>
+            </Container>
         </AuthenticatedLayout>
     );
 }
