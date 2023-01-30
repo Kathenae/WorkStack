83883768ceb5281c583e759074c7e1b74fb11c87
Ronald <RonaldWilsonMachado@gmail.com>
2023-01-30 16:03:31 +0200

    Implemented `job.proposals.index`

--- resources/js/Pages/Proposals/Index.jsx
+++ resources/js/Pages/Proposals/Index.jsx
@@ -1,19 +1,24 @@
 import Card from '@/Components/Card';
+import Conditional from '@/Components/Conditional';
 import Container from '@/Components/Container';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from "@inertiajs/react";
-import React from "react";
+import { Head, Link } from "@inertiajs/react";
+import React, { useState } from "react";
+import JobProposals from '@/Components/JobProposals';
+import JobCard from '@/Components/JobCard';
 
 export default function Index({ proposals, job, auth, errors: auth_errors }) {
+
     return (
         <AuthenticatedLayout
             auth={auth}
             errors={auth_errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{job.title} - Proposals</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Proposals - {job.title}</h2>}
         >
-            <Head title="Create Proposal" />
+            <Head title="View Proposals" />
             <Container>
-
+                <JobCard job={job} />
+                <JobProposals proposals={proposals} />
             </Container>
         </ AuthenticatedLayout >
     )
